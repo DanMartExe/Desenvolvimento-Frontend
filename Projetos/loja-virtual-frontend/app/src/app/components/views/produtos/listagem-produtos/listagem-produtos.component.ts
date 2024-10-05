1d8993435d3765b3ac9b6b72147502b68f28a146
DanMartExe <daniel.martins1@aluno.ifsp.edu.br>
2024-10-05 13:14:22 -0300

    adicionando projeto

---
+++ Projetos/loja-virtual-frontend/app/src/app/components/views/produtos/listagem-produtos/listagem-produtos.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ProdutoService } from '../../../../services/produto.service';
+import { Produto } from '../../../../models/produto';
+
+@Component({
+  selector: 'app-listagem-produtos',
+  templateUrl: './listagem-produtos.component.html',
+  styleUrl: './listagem-produtos.component.css'
+})
+export class ListagemProdutosComponent implements OnInit {
+  public produtos!: Produto[];
+produto: any;
+
+  constructor(private produtoService: ProdutoService) { }
+  ngOnInit(): void {
+    this.buscarProdutos()
+  }
+
+  public buscarProdutos() {
+    this.produtoService.buscarProdutos().subscribe(responde => {
+      this.produtos = responde;
+      console.log(this.produtos)
+    })
+  }
+
+  public deletarProduto(id: string) {
+    this.produtoService.deletarProduto(id).subscribe(response => {
+      console.log(response);
+      this.buscarProdutos();
+    })
+  }
+}
---
+++ Projetos/loja-virtual-frontend/loja-virtual.md
@@ -0,0 +1,11 @@
+# Criando loja virtual com Angular e Spring Boot
+
+### Criando app angular
+    ng new app --no-standalone --skip-tests
+
+### Comando usados em aula
+    ng g c components/shared/navbar
+    ng g c components/views/produtos/listagem-produtos
+    ng g c components/views/produtos/adicionar-produtos
+
+http://localhost:8080/swagger-ui/index.html
